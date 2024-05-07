2b96cac8987b8421b5087a0778fcac2dea427049
Dhiraj Acharya <076bct023.dhiraj@pcampus.edu.np>
2024-05-07 07:02:19 +0545

    added individual day's graph

--- script.js
+++ script.js
@@ -7,55 +7,23 @@ const inputBox = document.getElementById("input-box");
 const inputBtn = document.getElementById("input-btn");
 
 const graphBox = document.querySelector(".graph-box");
+const buttonGrp = document.querySelector(".button-grp");
+
+const allDay = document.getElementById("all-day");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
 let cityName = "";
 let minTemperature = 0;
 let maxTemperature = 0;
 
-
-// start: fetch data using openweather api
-
-const weatherData = async () => {
-    cityName = inputBox.value;
-    let dateTempMap = {};
-
-    const response = await fetch(url + `&q=${cityName}` + `&appid=${key}`);
-    const data = await response.json();
-
-
-    let lists = data.list;
-
-    minTemperature = 200;
-    maxTemperature = -200;
-    lists.forEach((list) => {
-
-        // console.log(list.main.temp, list.dt_txt);
-
-        dateTempMap[list.dt_txt] = list.main.temp;
-        if (list.main.temp < minTemperature) {
-            minTemperature = list.main.temp;
-        }
-
-        if (list.main.temp > maxTemperature) {
-            maxTemperature = list.main.temp;
-
-        }
-
-    });
-
-    // console.log(dateTempMap);
-    drawgraph(dateTempMap);
-    displayData(minTemperature, maxTemperature);
-
-}
-
-// end: fetch data using openweather api 
-
-
+// search box and search button related
 inputBtn.addEventListener("click", () => {
     if (inputBox.value !== "") {
         graphBox.style.display = "block";
-        weatherData()
+        buttonGrp.style.display = "flex";
+
+        weatherData();
     }
 })
 
@@ -63,49 +31,151 @@ inputBox.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         if (inputBox.value !== "") {
             graphBox.style.display = "block";
+            buttonGrp.style.display = "flex";
+
             weatherData();
         }
     }
 })
 
-function displayData(minT, maxT) {
-    const cityTitle = document.querySelector(".city-title p");
-    const weatherReport = document.querySelector(".weather-report");
-    const minTemperature = document.querySelector(".min-temperature p span");
-    const maxTemperature = document.querySelector(".max-temperature p span");
+
+// fetch data using openweather api
+const weatherData = async () => {
+    cityName = inputBox.value;
+    let count = 0;
+
+    const response = await fetch(url + `&q=${cityName}` + `&appid=${key}`);
+    const data = await response.json();
 
 
-    weatherReport.style.display = "block";
+    let lists = data.list;
 
-    cityTitle.innerText = inputBox.value.toUpperCase();
-    minTemperature.innerText = minT.toFixed(2) + " °C";
-    maxTemperature.innerText = maxT.toFixed(2) + " °C";
+    let dateTempMap = toDateTempObj(lists);
+    let dateTempArray = convertToArray(dateTempMap);
+
+    console.log(dateTempMap);
+    console.log(dateTempArray);
+
+    [minTemperature, maxTemperature] = getTempValue(dateTempArray[count]);
+    drawgraph(dateTempArray[count]);
+    displayData(minTemperature, maxTemperature);
+    count++;
+
+    allDay.addEventListener("click", () => {
+        [minTemperature, maxTemperature] = getTempValue(dateTempMap);
+        drawgraph(dateTempMap, "of all days");
+        displayData(minTemperature, maxTemperature);
+        count = 1;
+    })
+
+    next.addEventListener("click", ()=>{
+        if(count>=0 && count<dateTempArray.length){
+            [minTemperature, maxTemperature] = getTempValue(dateTempArray[count]);
+            drawgraph(dateTempArray[count]);
+            displayData(minTemperature, maxTemperature);
+            count++;
+        }
+    })
+
+    previous.addEventListener("click", ()=>{
+        if(count>0 && count<=dateTempArray.length){
+            [minTemperature, maxTemperature] = getTempValue(dateTempArray[count-1]);
+            drawgraph(dateTempArray[count-1]);
+            displayData(minTemperature, maxTemperature);
+            count--;
+        }
+    })
+
+}
+
+// for converting data lists into single object
+function toDateTempObj(lists) {
+    let dateTempMap = {};
+    lists.forEach((list) => {
+        dateTempMap[list.dt_txt] = list.main.temp;
+    });
+
+    return dateTempMap;
 }
 
 
+// for converting data into array of object
+function convertToArray(dateTempMap) {
+    let groupedObjects = [];
+    let currentDate = null;
+    let currentGroup = {};
 
-// start: draw graph
-function drawgraph(elem) {
+    for (const key in dateTempMap) {
+        const cdate = key.split(' ')[0];
+        if (cdate !== currentDate) {
+            if (Object.keys(currentGroup).length !== 0) {
+                groupedObjects.push(currentGroup);
+            }
+            currentGroup = {};
+            currentDate = cdate;
+        }
+        currentGroup[key] = dateTempMap[key];
+    }
+
+    if (Object.keys(currentGroup).length !== 0) {
+        groupedObjects.push(currentGroup);
+    }
+
+    return groupedObjects;
+}
+
+//for getting minimum and maximum temperature value
+function getTempValue(customObject) {
+    minTemperature = 200;
+    maxTemperature = -200;
+
+    for (let key in customObject) {
+        if (customObject[key] < minTemperature) {
+            minTemperature = customObject[key];
+        }
+
+        if (customObject[key] > maxTemperature) {
+            maxTemperature = customObject[key];
+        }
+    }
+
+    return [minTemperature, maxTemperature];
+
+
+}
+// for drawing graph
+function drawgraph(elem, lT="") {
 
     const ctx = document.getElementById('myChart');
 
-    let myLabel = [];
+    let labelText;
+    if(lT===""){
+        for(let key in elem){
+            labelText ="on "+key.split(' ')[0]; //for getting the first date of an object
+            break;
+        }
+    }else{
+        labelText = lT;
+    }
+
+    let xAxisName = [];
     let myData = [];
 
     for (let key in elem) {
-        myLabel.push(key);
+        xAxisName.push(key);
         myData.push(elem[key]);
     }
 
     const data = {
         // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        labels: myLabel,
+        labels: xAxisName,
         datasets: [{
-            label: 'Temperature',
+            label: `Temperature graph ${labelText}`,
             // data: [65, 59, 80, 81, 56, 55, 40],
             data: myData,
             fill: true,
-            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgba(215,226,244,0.6)',
+            borderColor: 'rgb(51,206,152)',
             tension: 0.1
         }]
     };
@@ -122,4 +192,18 @@ function drawgraph(elem) {
     });
 
 }
-// end: draw graph
+
+//for displaying data inside weather-report
+function displayData(minT, maxT) {
+    const cityTitle = document.querySelector(".city-title p");
+    const weatherReport = document.querySelector(".weather-report");
+    const minTemperature = document.querySelector(".min-temperature p span");
+    const maxTemperature = document.querySelector(".max-temperature p span");
+
+
+    weatherReport.style.display = "block";
+
+    cityTitle.innerText = inputBox.value.toUpperCase();
+    minTemperature.innerText = minT.toFixed(2) + " °C";
+    maxTemperature.innerText = maxT.toFixed(2) + " °C";
+}
